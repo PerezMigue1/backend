82a005e0775091fec75d0e0a447c1559a21ca7f8
PerezMigue1 <20230091@uthh.edu.mx>
2025-06-24 20:53:29 -0600

    7 commit

--- api/index.js
+++ api/index.js
@@ -18,7 +18,7 @@ app.use(cors({
 }));
 
 // Conexión a MongoDB Atlas
-mongoose.connect(process.env.MONGODB_URI, {
+mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true
 })
