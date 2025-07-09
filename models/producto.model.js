824334c93dcf0c96c76ef816efb0c0c4fda392db
PerezMigue1 <20230091@uthh.edu.mx>
2025-07-09 09:12:28 -0600

    commit

--- models/producto.model.js
+++ models/producto.model.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const ProductoSchema = new mongoose.Schema({
     idProducto: { type: String, required: true, unique: true },
     Nombre: { type: String, required: true },
-    Imagen: { type: String, required: true },
+    Imagen: [{ type: String, required: true }],
     Precio: { type: Number, required: true },
     Descripción: { type: String },
     Forma: { type: String },
