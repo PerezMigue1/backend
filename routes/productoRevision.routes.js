f7f6f7f170a64985fa305b13946988ed03276cda
PerezMigue1 <20230091@uthh.edu.mx>
2025-07-18 01:00:18 -0600

    commit9

--- routes/productoRevision.routes.js
+++ routes/productoRevision.routes.js
@@ -3,26 +3,8 @@ const router = express.Router();
 const controller = require('../controllers/productoRevision.controller');
 const upload = require('../middlewares/uploadCloudinary.middleware');
 
-const { verificarToken, permitirRoles } = require("../middlewares/auth.middleware"); // verifica token
-
-// Aprobar producto (solo admin)
-router.put("/aprobar/:id", verificarToken, permitirRoles("admin"),
-    controller.aprobarProducto
-);
-
-// Rechazar producto (solo admin)
-router.delete("/rechazar/:id", 
-    verificarToken,
-    permitirRoles("admin"),
-    controller.rechazarProducto
-);
-
-// Obtener productos en revisión
-router.get('/', 
-    controller.obtenerTodo
-);
-
 router.post('/', upload.array('Imagen'), controller.crear);
+router.get('/', controller.obtenerTodos);
 router.get('/:id', controller.obtenerPorId);
 router.put('/:id', controller.actualizarProducto);
 router.delete('/:id', controller.eliminarProducto);
